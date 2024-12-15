f11547d9b22500a15fa3586dc6d4aa4f6677e2c2
Kunal Sarkar <kunalsarkar6290@gmail.com>
2024-12-15 22:09:34 +0530

    index.css deleted

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-// import Main from './App.js';
 import Loading from "./Loading.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
